b237b435cd6e38f38cdc50312c46dd0af761d72d
Sayan Ghorui <sayanghorui611@gmail.com>
2023-09-24 12:53:40 +0530

    Create minimumDifference.cpp

---
+++ 23.09.2023/Chocolate Distribution /C++ /minimumDifference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public :
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        sort(a.begin(), a.end());
+        int i=0,j=m-1;
+        long long ans=INT_MAX;
+        while(j<n){
+            ans = min(ans, a[j]-a[i]);
+            i++;j++;
+        }
+        return ans;
+}
